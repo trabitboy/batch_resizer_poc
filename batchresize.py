eed70e50244145209f84f02f0ef5854a7e76fdb6
trabitboy <trabitboy@gmail.com>
2018-03-08 12:28:44 +0100

    selection save and scale

--- batchresize.py
+++ batchresize.py
@@ -1,43 +1,75 @@
 # 15 min
 # +10 min
 # +20 min
+# + 40 min
+
+#TODO
+# scale down picture larger than viewport,
+# determine zoom level
+# select based on zoom level
+# navigate to next slot
 
 #poc for the great M
 import pygame
 from pygame.locals import *
 
 
-WIDTH=640
-HEIGHT=480
+WIDTH=800
+HEIGHT=600
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT))
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
+BASICFONT = pygame.font.Font('freesansbold.ttf', 32)
+
 selection=pygame.Rect(100,100,200,200);
+
 
 
 #poc, pic list here
 piclist=["test.bmp","test2.bmp","test3.bmp"]
 
-currentpicname=piclist[1]
+num=0
+currentpicname=piclist[num]
+print(currentpicname)
+
+msg = BASICFONT.render(currentpicname, True, RED)
 
 currentsurf=pygame.image.load(currentpicname)
 
-bgRect=pygame.Rect(0,0,640,480);
+bgRect=pygame.Rect(0,0,WIDTH,HEIGHT);
 
+##tmpBeforeScale=None
 
 def goToNextPic():
     print("saving then changing slot")
+    tmpBeforeScale=pygame.Surface((selection.w,selection.w))
+    tmpBeforeScale.blit(currentsurf,
+                        (0,0),
+                        selection
+                         )
+    pygame.image.save(tmpBeforeScale,"outbefscale.bmp")    
+##    tmpScaled=pygam
+    tmpScaled=pygame.transform.scale(tmpBeforeScale,(600,600))
+    pygame.image.save(tmpScaled,"outscaled.bmp")
+    
     #TODO resize save
-
+##    pygame.transform.scale()
+    
     #TODO move slot
+##    currentpicname=piclist[num]
+##
+##    msg = BASICFONT.render(currentpicname, True, RED)
+##
+##    currentsurf=pygame.image.load(currentpicname)
 
 def displayUI():
     DISPLAYSURF.fill(WHITE)
     DISPLAYSURF.blit(currentsurf,bgRect)
     pygame.draw.rect(DISPLAYSURF,RED,selection,3)
+    DISPLAYSURF.blit(msg,(0,0))
     pygame.display.update()
 
 running=True
@@ -59,24 +91,25 @@ while running:
             
         elif event.type == MOUSEMOTION:
             print("mouse button move")
-    ##        self.quit=True
-            #terminate()
-    # Possible joystick actions: JOYAXISMOTION JOYBALLMOTION JOYBUTTONDOWN JOYBUTTONUP JOYHATMOTION
-    #            elif event.type == JOYBUTTONDOWN:
-            #print("Joystick button pressed.")
-    #           elif event.type == pygame.JOYBUTTONUP:
-            #print("Joystick button released.")
+            pos=pygame.mouse.get_pos()
+            tw=pos[0]-selection.x
+            th=pos[1]-selection.y
+
+            tgt=None
+            if tw>=th :
+                tgt=tw
+            else:
+                tgt=th
+            selection.h=tgt
+            selection.w=tgt
         elif event.type == MOUSEBUTTONUP:
             print("mouse button up")
-            pos=pygame.mouse.get_pos()
-            selection.w=pos[0]-selection.x
-            selection.h=pos[1]-selection.y
+##            pos=pygame.mouse.get_pos()
+##            selection.w=pos[0]-selection.x
+##            selection.h=pos[1]-selection.y
         elif event.type == KEYDOWN:
-            if event.key == ( K_f):
+            if event.key == ( K_SPACE):
                 goToNextPic()
-#                send_to_arduino(ser,'f')
-##            elif event.key == (K_j):
-##                send_to_arduino(ser,'j')
 
 
 

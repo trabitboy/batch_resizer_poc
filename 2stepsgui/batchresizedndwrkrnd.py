42b71b7453f1a0a5c3229b611f28daeb90c0f0ac
trabitboy <trabitboy@gmail.com>
2018-03-13 22:22:30 +0100

    task list

--- 2stepsgui/batchresizedndwrkrnd.py
+++ 2stepsgui/batchresizedndwrkrnd.py
@@ -1,3 +1,17 @@
+#dnd to pyg with a button 3
+#resize of window 3
+#zooming and navigating the zoom mousewheel centré du pointeur de la souris 1
+#backup last image and revert on right/middle click
+#no initial selection1
+#click to start selection 1
+#save and crop on release1
+#cancel selection in progress with another mouse button 3
+#support pictures bigger than window 2
+#make 3 different crops with buttons in wx widgets part 2
+#save over source picture 1
+
+
+
 # 15 min
 # +10 min
 # +20 min
